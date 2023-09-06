d7c68f02772f12595cf1bbaba342fd4958b9f420
Hong Seong Mok <swff07183@gmail.com>
2023-09-07 00:50:31 +0900

    [Feat] SideMenu toggle 기능 추가

---
+++ src/recoil/extra/hooks.ts
@@ -0,0 +1,49 @@
+import { useRecoilState } from 'recoil';
+import { extraFormRecoilState, extraListRecoilState } from './atom';
+import { ExtraMenuType } from '../../models/common';
+
+export const useExtraState = () => {
+  const [extraListState, setExtraListState] =
+    useRecoilState(extraListRecoilState);
+  const [extraFormState, setExtraFormState] =
+    useRecoilState(extraFormRecoilState);
+
+  const openExtraList = (key: ExtraMenuType) =>
+    setExtraListState({
+      ...extraListState,
+      [key]: true,
+    });
+
+  const closeExtraList = (key: ExtraMenuType) =>
+    setExtraListState({
+      ...extraListState,
+      [key]: false,
+    });
+
+  const toggleExtraList = (key: ExtraMenuType) =>
+    setExtraListState((prev) => ({
+      ...extraListState,
+      [key]: !prev[key],
+    }));
+
+  const openExtraForm = (key: ExtraMenuType) =>
+    setExtraFormState({
+      ...extraFormState,
+      [key]: true,
+    });
+  const closeExtraForm = (key: ExtraMenuType) =>
+    setExtraFormState({
+      ...extraFormState,
+      [key]: false,
+    });
+
+  return {
+    extraListState,
+    openExtraList,
+    closeExtraList,
+    toggleExtraList,
+    extraFormState,
+    openExtraForm,
+    closeExtraForm,
+  };
+};
