c3769761a54d76f7ff1f729f132256c1865e1b64
Hong Seong Mok <swff07183@gmail.com>
2023-10-24 21:18:27 +0900

    build

--- front/src/api/client.ts
+++ front/src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-export const BASE_URL = 'http://localhost:8000/api';
-// export const BASE_URL = 'https://ivyleaguer-resume.site/api';
+// export const BASE_URL = 'http://localhost:8000/api';
+export const BASE_URL = 'https://ivyleaguer-resume.site/api';
 
 export const client = axios.create({
   url: BASE_URL,
